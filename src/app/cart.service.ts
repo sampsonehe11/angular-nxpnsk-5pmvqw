2de306dea57d8769518b915dc8715100aec85b53
sampsonehe11 <shaoyin.he@outlook.com>
2021-10-10 14:36:56 +0800

    2 files modified

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Product } from './products';
 
-@Injectable()
+@Injectable({
+  providedIn: 'root',
+})
 export class CartService {
   items: Product[] = [];
   /* . . . */
