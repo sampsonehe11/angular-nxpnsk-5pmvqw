eb723f73d4eb38ccf97da42c4c4e5a532024bfe5
sampsonehe11 <shaoyin.he@outlook.com>
2021-10-10 14:23:15 +0800

    added cart service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
       { path: 'products/:productId', component: ProductDetailsComponent },
+      { path: 'cart', component: CartComponent },
     ]),
   ],
   declarations: [
