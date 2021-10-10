d2456dcb4352470ae0426fa9683e33b4b1e635f0
sampsonehe11 <shaoyin.he@outlook.com>
2021-10-10 14:24:13 +0800

    2 files modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CartService } from '../cart.service';
 import { Product, products } from '../products';
 
 @Component({
